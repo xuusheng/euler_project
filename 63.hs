7a28dcc7105e63a65b271a00adbd558438131130
xusheng <xuusheng@126.com>
2015-06-13 20:17:52 +0800

    add

--- 63.hs
+++ 63.hs
@@ -4,4 +4,4 @@
 digits 0 = []
 digits x = x `mod` 10 : digits (x `div` 10)
 
-length $concat $ map (\x -> (filter (\y->length(digits(y)) == x) (map (^x) [1..9]))) [1..21]
+result=length $concat $ map (\x -> (filter (\y->length(digits(y)) == x) (map (^x) [1..9]))) [1..21]
